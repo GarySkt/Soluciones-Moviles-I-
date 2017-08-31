9bf4e39ab95624746fd7ac5dd5371729439ef29c
Gary Calle <gary.sk8@icloud.com>
2017-08-31 11:39:45 -0500

    food truck
    initial

---
+++ food_truck/app/src/main/java/com/example/gary/food_truck/Recepcion_mensaje.java
@@ -0,0 +1,13 @@
+package com.example.gary.food_truck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recepcion_mensaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recepcion_mensaje);
+    }
+}
