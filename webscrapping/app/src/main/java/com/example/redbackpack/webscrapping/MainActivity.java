ff9dd9ad9c219b602824dd6b50fd2caf1271e826
Gary Calle <gary.sk8@icloud.com>
2018-06-13 10:53:05 -0500

    webscrapingminedu
    moviles I examen II unidad

---
+++ webscrapping/app/src/main/java/com/example/redbackpack/webscrapping/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.redbackpack.webscrapping;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.EventLogTags;
+
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressDialog mProgressDialog;
+    private String url = "http://www.minedu.gob.pe/n/archivo.php";
+    private ArrayList<String> mAuthorNameList = new ArrayList<>();
+    private ArrayList<String> mBlogUploadDateList = new ArrayList<>();
+    private ArrayList<String> mBlogTitleList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Description().execute();
+
+    }
+
+    private class Description extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("MINEDU");
+            mProgressDialog.setMessage("Cargando...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                org.jsoup.nodes.Document mBlogDocument = Jsoup.connect(url).get();
+                // Using Elements to get the Meta data
+                Elements mElementDataSize = mBlogDocument.select("article[class=item_agenda]");
+                // Locate the content attribute
+                int mElementSize = mElementDataSize.size();
+
+                for (int i = 0; i < mElementSize; i++) {
+                    Elements mElementAuthorName = mBlogDocument.select("div[class=detalle_agenda]").select("div[class=desc]").select("p").eq(i);
+                    String mAuthorName = mElementAuthorName.text();
+
+                    Elements mElementBlogUploadDate = mBlogDocument.select("div[class=head_agenda]").select("div[class=option]").select("i").eq(i);
+                    String mBlogUploadDate = mElementBlogUploadDate.text();
+
+                    Elements mElementBlogTitle = mBlogDocument.select("div[class=head_agenda]").select("h3").select("a").eq(i);
+                    String mBlogTitle = mElementBlogTitle.text();
+
+                    mAuthorNameList.add(mAuthorName);
+                    mBlogUploadDateList.add(mBlogUploadDate);
+                    mBlogTitleList.add(mBlogTitle);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set description into TextView
+
+            RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.act_recyclerview);
+
+            DataAdapter mDataAdapter = new DataAdapter(MainActivity.this, mBlogTitleList, mAuthorNameList, mBlogUploadDateList);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+            mRecyclerView.setLayoutManager(mLayoutManager);
+            mRecyclerView.setAdapter(mDataAdapter);
+
+            mProgressDialog.dismiss();
+        }
+    }
+}
